4b186ef308b9f249a264f76ff8795d3fe4427250
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2023-07-02 20:24:52 +0700

    Remove main

--- Data Scraping/src/getInfo.py
+++ Data Scraping/src/getInfo.py
@@ -92,18 +92,3 @@ def scrape_manga_data():
     manga_data = [manga.to_dict() for manga in manga_objects]
 
     return manga_data
-
-if __name__ == "__main__":
-    start_time = time.time()
-
-    # Scrape manga data
-    all_manga_data = scrape_manga_data()
-
-    # Convert the Python objects into JSON and export it to manga.json file.
-    with open('manga_list.json', 'w', encoding='utf-8') as f:
-        json.dump(all_manga_data, f, indent=4, ensure_ascii=False)
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-
-    print(f"Created manga_list.json file in {execution_time} seconds.")
